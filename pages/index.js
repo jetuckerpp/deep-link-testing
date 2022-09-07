ef6574f2a31c6cbc4f7a9f3b64c2d751abef2edb
Jesse Tucker <jetucker@paypal.com>
2022-09-07 17:25:57 -0400

    Add deep link examples

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,39 @@
 import Link from 'next/link'
 const Index = () => (
   <div>
-    Hello World.{' '} This is a test.{' '}
+    <h1>Deep Link Testing</h1>
     <Link href="/about">
-      <a>About</a>
+      <a>About (local)</a>
     </Link>
+    <br/>
+    <Link href="https://www.google.com">
+      <a>Google (external https)</a>
+    </Link>
+    <br/>
+    <Link href="https://venmo.com">
+      <a>Venmo (external https)</a>
+    </Link>
+    <br/>
+    <Link href="mailto:jetucker@paypal.com">
+      <a>mailto jetucker@paypal.com</a>
+    </Link>
+    <hr />
+    <Link href="https://amazon.com/dp/B077MZ2MPW">
+      <a>https://amazon.com/dp/B077MZ2MPW</a>
+    </Link>
+    <br/>
+    <Link href="com.amazon.mobile.shopping.web://amazon.com/dp/B077MZ2MPW">
+      <a>com.amazon.mobile.shopping.web://amazon.com/dp/B077MZ2MPW</a>
+    </Link>
+    <br/>
+    <Link href="com.amazon.mobile.shopping.web:/amazon.com/dp/B077MZ2MPW">
+      <a>com.amazon.mobile.shopping.web:/amazon.com/dp/B077MZ2MPW</a>
+    </Link>
+    <br/>
+    <Link href="com.amazon.mobile.shopping.web:amazon.com/dp/B077MZ2MPW">
+      <a>com.amazon.mobile.shopping.web:amazon.com/dp/B077MZ2MPW</a>
+    </Link>
+    
   </div>
 )
 export default Index;
